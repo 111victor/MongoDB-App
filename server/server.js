a3589e44c912f7f3e83c14d8beb0bc2cd0f2651b
qingyang <1234567victor@163.com>
2018-11-01 23:24:31 -0700

    Add GET /users/me and auth

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _=require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -5,8 +6,13 @@ var {mongoose} = require('./db/mongoose.js')
 
 var{Todo} = require('./models/todo');
 
+var{User} = require('./models/user');
+
+var{authenticate} = require('./middleware/authenticate');
 
 const {ObjectID} = require('mongodb');
+
+
 
 // var{User} = require('./models/user');
 
@@ -36,6 +42,32 @@ app.get('/todos',(req,res)=>{
 	})
 });
 
+app.patch('/todos/:id',(req,res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text','completed']);
+
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+	if(_.isBoolean(body.completed)&& body.completed){
+		body.completedAt = new Date().getTime();
+	}else{
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id,{$set: body},{new:true}).then((todo) => {
+		if(!todo){
+			return res.status(404).send();
+		}
+		res.send(todo);
+	}).catch((e) => {
+		res.status(400).send();
+	})
+});
+
+
+
 app.get('/todos/:id',(req,res) => {
 	var id = req.params.id;
 	if(!ObjectID.isValid(id)){
@@ -50,8 +82,30 @@ app.get('/todos/:id',(req,res) => {
 	}).catch((e)=>res.status(404).send());
 });
 
+app.post('/users',(req,res) =>{
+	var body = _.pick(req.body, ['email', 'password']);
+	var user = new User(body);
+
+	user.save().then(() =>{
+		return user.generateAuthToken();
+	}).then((token)=>{
+		res.header('x-auth',token).send(user);
+	}).catch((e) => {
+		res.status(400).send(e);
+	})
+});
+
+
+
+
+app.get('/users/me',authenticate, (req,res) => {
+	res.send(req.user);
+});
+
 
 
 app.listen(port, () =>{
 	console.log(`Started on port ${port}`);
 });
+
+module.exports = {app}
